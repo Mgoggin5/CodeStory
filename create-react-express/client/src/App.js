20dd4a5a97b482f59b57ef637086fe7f6e28047e
Alex Scarlett <alex.scarlett91@gmail.com>
2018-05-16 13:29:46 -0400

    Installed react-router

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Stories from './components/Stories';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/stories" component={Stories} />
+          </Switch>
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
